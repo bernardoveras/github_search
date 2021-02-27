ffe0e29bc2ff034d14076a5b7dc80d6268a410c2
Bernardo Veras <bernardo@vvssistemas.com.br>
2021-02-27 08:50:39 -0300

    Add json key's in documentation in UserModel

--- lib/data/models/user_model.dart
+++ lib/data/models/user_model.dart
@@ -4,15 +4,25 @@ import 'package:kayta/services/http/errors/http_errors.dart';
 
 class UserModel extends User {
   UserModel({
+    /// [name]
     @required String name,
+    /// [login]
     @required String user,
+    /// [company]
     @required String company,
+    /// [bio]
     @required String bio,
+    /// [email]
     @required String email,
+    /// [location]
     @required String location,
+    /// [avatar_url]
     @required String avatarUrl,
+    /// [followers]
     @required int followers,
+    /// [following]
     @required int following,
+    /// [public_repos]
     @required int publicRepos,
     int totalStars,
   }) : super(
